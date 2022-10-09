697518519569419d328e7ec8e29f1c5571383267
Mingjia Jett Chen <mjchen@islandcompanies.com>
2022-10-09 16:15:27 -0400

    added copy tool and pdf tool

---
+++ Utility/Copy_File_From_To.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.GUI;
+using Rhino.Geometry;
+
+namespace IEF_Toolbox.Utility
+{
+    public class Copy_File_From_To : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the Boolean_Difference_Slow class.
+        /// </summary>
+        public Copy_File_From_To()
+          : base("Copy File From To", "Copy",
+              "Copy file from a sourcePath to a targetPath based on partNumber and fileExtension",
+              "IEF Toolbox", "01_Utility")
+        {
+        }
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.septenary; }
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBrepParameter("partNumber", "pN", "List of parts to run the copy", GH_ParamAccess.item);
+            pManager.AddBrepParameter("fileExtension", "ext", "file extension", GH_ParamAccess.item);
+            pManager.AddBrepParameter("sourcePath", "sP", "source path", GH_ParamAccess.item);
+            pManager.AddBrepParameter("targetPath", "tP", "target path", GH_ParamAccess.item);
+            pManager.AddBrepParameter("run", "run", "Turn to True to activate the copy", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBrepParameter("outputMessage", "message", "message", GH_ParamAccess.item);
+        }
+
+
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string pN = null;
+            string sourceDir = null;
+            string targetDir = null;
+            string ext = null;
+            bool run = false;
+
+            bool success0 = DA.GetData<string>(0, ref pN);
+            bool success1 = DA.GetData<string>(1, ref ext);
+            bool success2 = DA.GetData<string>(2, ref sourceDir);
+            bool success3 = DA.GetData<string>(3, ref targetDir);
+            bool success4 = DA.GetData<bool>(4, ref run);
+
+            string outputMessage = null;
+
+            string sDir = sourceDir + "\\" + pN + ext;
+            string tDir = targetDir + "\\" + pN + ext;
+
+
+            
+            if (run){
+                // check if file under path exists // if not, create folder path
+
+                
+
+                File.Copy(sDir, tDir);
+            }
+
+            DA.SetData(0, outputMessage);
+        }
+
+        private bool Test_Folder_Exists(string folder_path)
+        {
+            bool folder_exist = false;
+
+            return folder_exist;
+        }
+
+        private bool Test_sourceFile_Exists(string file_path)
+        {
+            bool file_exist = false;
+
+            return file_exist;
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("D4F89E9F-D181-4ED4-87E2-EEC82505C6C0"); }
+
+
+        }
+
+    }
+}
+
---
+++ Utility/MergePDF.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace IEF_Toolbox.Utility
+{
+    class MergePDF_by_part : GH_Component
+    {        /// <summary>
+             /// Initializes a new instance of the Boolean_Difference_Slow class.
+             /// </summary>
+        public MergePDF_by_part()
+          : base("MergePDF by Part", "MergePDF by Part",
+              "Look for the individual PDFs in the given directories, combine the PDF sheets, and export a single PDF per Part to the taget directory",
+              "IEF Toolbox", "01_Utility")
+        {
+        }
+
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.septenary; }
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBrepParameter("partNumber", "pN", "List of parts to run the copy", GH_ParamAccess.item);
+            pManager.AddBrepParameter("sourceFolderPath", "sP", "source path", GH_ParamAccess.list);
+            pManager.AddBrepParameter("targetFolderPath", "tP", "target path", GH_ParamAccess.item);
+            pManager.AddBrepParameter("run", "run", "Turn to True to activate the copy", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBrepParameter("outputMessage", "message", "message", GH_ParamAccess.item);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string pN = null;
+            List<string> sourceFolderDir = new List<string>();
+            string targetFolderDir = null;
+            bool run = false;
+            string outputMessage = null;
+
+            bool success0 = DA.GetData<string>(0, ref pN);
+            bool success1 = DA.GetDataList<string>(1, sourceFolderDir) ;
+            bool success2 = DA.GetData<string>(2, ref targetFolderDir);
+            bool success3 = DA.GetData<bool>(3, ref run);
+
+            // check if the pN pdf exist in the folder directory
+
+
+            // get file path from inputs
+            List<string> sourceDir = new List<string>();
+            foreach(string dir in sourceFolderDir) { sourceDir.Add(dir + "\\" + pN + ".pdf"); }
+            string targetDir = targetFolderDir + "\\" + pN + ".pdf";
+
+            // run the merge
+            if (run) { MergePDFs(targetDir, sourceDir); }
+
+            DA.SetData(0, outputMessage);
+        }
+
+        private static void MergePDFs(string targetPath, List<string> pdfs)
+        {
+            using (var targetDoc = new PdfDocument())
+            {
+                foreach (var pdf in pdfs)
+                {
+                    using (var pdfDoc = PdfReader.Open(pdf, PdfDocumentOpenMode.Import))
+                    {
+                        for (var i = 0; i < pdfDoc.PageCount; i++)
+                            targetDoc.AddPage(pdfDoc.Pages[i]);
+                    }
+                }
+                targetDoc.Save(targetPath);
+            }
+        }
+
+
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("4B1D24B0 - 7EBC - 4F04 - 9B12 - DB48A3AE3823"); }
+        }
+
+    }
+}
